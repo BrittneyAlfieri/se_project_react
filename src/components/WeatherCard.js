7f06ae67aaceaaa0a5f2b886d0d1c45e79ca09b2
Brittney Alfieri <vbrit95@hotmail.com>
2023-03-29 21:24:27 -0400

    Project 10 intial commits

--- src/components/WeatherCard.js
+++ src/components/WeatherCard.js
@@ -1,65 +1,31 @@
+import React from "react";
+import sunnyDay from "../images/day/sunny-day.png";
+import cloudyDay from "../images/day/cloudy-day.png";
+import fogDay from "../images/day/fog-day.png";
+import rainDay from "../images/day/rain-day.png";
+import snowDay from "../images/day/snow-day.png";
+import stormDay from "../images/day/storm-day.png";
+import sunnyNight from "../images/night/sunny-night.png";
+import cloudyNight from "../images/night/cloudy-night.png";
+import fogNight from "../images/night/fog-night.png";
+import rainNight from "../images/night/rain-night.png";
+import snowNight from "../images/night/snow-night.png";
+import stormNight from "../images/night/storm-night.png";
+
 const WeatherCard = ({ day, type, weatherTemp = "" }) => {
   const weatherOptions = [
-    {
-      url: require("../images/day/sunny-day.png").default,
-      day: true,
-      type: "sunny",
-    },
-    {
-      url: require("../images/day/cloudy-day.png").default,
-      day: true,
-      type: "cloudy",
-    },
-    {
-      url: require("../images/day/fog-day.png").default,
-      day: true,
-      type: "fog",
-    },
-    {
-      url: require("../images/day/rain-day.png").default,
-      day: true,
-      type: "rain",
-    },
-    {
-      url: require("../images/day/snow-day.png").default,
-      day: true,
-      type: "snow",
-    },
-    {
-      url: require("../images/day/storm-day.png").default,
-      day: true,
-      type: "storm",
-    },
-    {
-      url: require("../images/night/sunny-night.png").default,
-      day: false,
-      type: "sunny",
-    },
-    {
-      url: require("../images/night/cloudy-night.png").default,
-      day: false,
-      type: "cloudy",
-    },
-    {
-      url: require("../images/night/fog-night.png").default,
-      day: false,
-      type: "fog",
-    },
-    {
-      url: require("../images/night/rain-night.png").default,
-      day: false,
-      type: "rain",
-    },
-    {
-      url: require("../images/night/snow-night.png").default,
-      day: false,
-      type: "snow",
-    },
-    {
-      url: require("../images/night/storm-night.png").default,
-      day: false,
-      type: "storm",
-    },
+    { url: sunnyDay, day: true, type: "sunny" },
+    { url: cloudyDay, day: true, type: "cloudy" },
+    { url: fogDay, day: true, type: "fog" },
+    { url: rainDay, day: true, type: "rain" },
+    { url: snowDay, day: true, type: "snow" },
+    { url: stormDay, day: true, type: "storm" },
+    { url: sunnyNight, day: false, type: "sunny" },
+    { url: cloudyNight, day: false, type: "cloudy" },
+    { url: fogNight, day: false, type: "fog" },
+    { url: rainNight, day: false, type: "rain" },
+    { url: snowNight, day: false, type: "snow" },
+    { url: stormNight, day: false, type: "storm" },
   ];
 
   const imageSrc = weatherOptions.filter((i) => {
