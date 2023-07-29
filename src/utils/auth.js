065bbca3291519ee928a93dfd426d78374878d51
Brittney Alfieri <vbrit95@hotmail.com>
2023-07-29 11:14:57 -0400

    project 15 initial commits

--- src/utils/auth.js
+++ src/utils/auth.js
@@ -1,7 +1,7 @@
 const BASE_URL =
   process.env.NODE_ENV === "production"
     ? "api.wtwr.wildsurf.net"
-    : "http://localhost:3000";
+    : "http://localhost:3001";
 
 const handleServerResponse = (res) => {
   if (res.ok) {
